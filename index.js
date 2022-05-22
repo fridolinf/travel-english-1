20d5f772a3737090b0cffc5c68496515684ed9e8
fridolinf <frelly130@gmail.com>
2022-05-23 01:07:35 +0800

    init packages.html

--- index.js
+++
@@ -1,5 +0,0 @@
-const nav1 = document.getElementsByClassName("nav1");
-const nav2 = document.getElementsByClassName("nav2");
-const nav3 = document.getElementsByClassName("nav3");
-
-console.log(nav1);
