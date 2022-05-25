dc43b2bd7f908b9c942f888dcdeb85a3d33dfabb
fridolinf <50565864+fridolinf@users.noreply.github.com>
2022-05-26 01:25:25 +0800

    init final

--- index.js
+++ index.js
@@ -33,3 +33,88 @@ function rights() {
 
   }
 }
+
+
+const btnScrollDown = document.querySelector('#mouse-indicator');
+function scrollDown() {
+  var windowCoords = document.documentElement.clientHeight;
+  (function scroll() {
+    if (window.pageYOffset < windowCoords) {
+      window.scrollBy(0, 10);
+      setTimeout(scroll, 0);
+      btnScrollDown2.classList.remove("deletes")
+      btnScrollDown.classList.add("deletes")
+    }
+  })();
+}
+
+btnScrollDown.addEventListener('click', scrollDown);
+
+const btnScrollDown2 = document.querySelector('#mouse-indicator-2');
+function scrollDown2() {
+  (function scroll() {
+    if (window.pageYOffset < 1600) {
+      window.scrollBy(0, 10);
+      setTimeout(scroll, 0);
+      btnScrollDown3.classList.remove("deletes")
+      btnScrollDown2.classList.add("deletes")
+    }
+  })();
+}
+
+btnScrollDown2.addEventListener('click', scrollDown2);
+
+
+const btnScrollDown3 = document.querySelector('#mouse-indicator-3');
+function scrollDown3() {
+  (function scroll() {
+    if (window.pageYOffset < 2300) {
+      window.scrollBy(0, 10);
+      setTimeout(scroll, 0);
+      btnScrollDown4.classList.remove("deletes")
+      btnScrollDown3.classList.add("deletes")
+    }
+  })();
+}
+
+btnScrollDown3.addEventListener('click', scrollDown3);
+
+
+const btnScrollDown4 = document.querySelector('#mouse-indicator-4');
+function scrollDown4() {
+  (function scroll() {
+    if (window.pageYOffset < 2900) {
+      window.scrollBy(0, 10);
+      setTimeout(scroll, 0);
+      btnScrollDown5.classList.remove("deletes")
+      btnScrollDown4.classList.add("deletes")
+    }
+  })();
+}
+
+btnScrollDown4.addEventListener('click', scrollDown4);
+
+const btnScrollDown5 = document.querySelector('#mouse-indicator-5');
+function scrollDown5() {
+  (function scroll() {
+    if (window.pageYOffset < 3900) {
+      window.scrollBy(0, 10);
+      setTimeout(scroll, 0);
+      btnScrollDown6.classList.remove("deletes")
+      btnScrollDown5.classList.add("deletes")
+    }
+  })();
+}
+
+btnScrollDown5.addEventListener('click', scrollDown5);
+
+
+const btnScrollDown6 = document.querySelector('#mouse-indicator-6');
+function scrollDown6() {
+      window.scrollTo({behavior: "smooth", top})
+      btnScrollDown.classList.remove("deletes")
+      btnScrollDown5.classList.add("deletes")
+      btnScrollDown6.classList.add("deletes")
+}
+
+btnScrollDown6.addEventListener('click', scrollDown6);
